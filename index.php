6ca95cf97fb5384094868dac54af7e17f4d4729f
HP-PD1\Enda <enda.setiyo@gmail.com>
2025-11-28 11:19:00 +0700

    test

--- index.php
+++ index.php
@@ -836,8 +836,8 @@ require_once __DIR__ . '/inc/header.php';
                 <?php if (!empty($partners)): ?>
                     <?php foreach ($partners as $pr): ?>
                     <div class="swiper-slide">
-                        <div class="partner-item partner-clickable" 
-                             <?= 'data-certificate="' . e($base . '/' . $pr['img_url']) . '" data-partner-name="' . e($pr['name']) . '"' ?>>
+                        <div class="partner-item <?= !empty($pr['img_url']) ? 'partner-clickable' : '' ?>" 
+                             <?= !empty($pr['img_url']) ? 'data-certificate="' . e($base . '/' . $pr['img_url']) . '" data-partner-name="' . e($pr['name']) . '"' : '' ?>>
                             <?php if (!empty($pr['logo_url'])): ?>
                                 <div class="partner-logo-img">
                                     <img src="<?= e($base . '/' . $pr['logo_url']) ?>" alt="<?= e($pr['name']) ?>">
